751f0f5591cd2272f5aa8fe0e6ab57588b0653af
Nils Hartmann <nils@nilshartmann.net>
2019-07-18 13:44:59 +0200

    Initialize with mounted 'false'?
    IMHO when the hook runs for the first time, the component is not mounted already, so the initial value should be false.
    
    Only after the (first) execution of useEffect it is mounted and isMounted should change to true.
    
    (Makes probably not a big difference in real life, but feels more correct)

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import { useRef, useEffect } from 'react';
 
 const useIsMounted = () => {
-  const isMounted = useRef(true);
-  useEffect(() => () => {
-    isMounted.current = false;
+  const isMounted = useRef(false);
+  useEffect(() => {
+    isMounted.current = true;
+    return () => isMounted.current = false;
   }, []);
   return isMounted;
 };
