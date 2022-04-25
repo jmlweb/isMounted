2d3458b0f168a358a6a4859c4ceab827ac34e159
José Manuel Lucas <josemanuel@jmlweb.es>
2022-04-25 21:15:57 +0200

    feat: upgrade libraries

--- index.es.js
+++ index.es.js
@@ -2,9 +2,12 @@ import { useRef, useEffect } from 'react';
 
 const useIsMounted = () => {
   const isMounted = useRef(false);
+
   useEffect(() => {
     isMounted.current = true;
-    return () => isMounted.current = false;
+    return () => {
+      isMounted.current = false
+    };
   }, []);
   return isMounted;
 };
